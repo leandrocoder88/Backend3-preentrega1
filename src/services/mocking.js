7f26c7537e3f784458296d5642d7e904d6af4ddd
Leandro Romegialli <leandrocodex88@gmail.com>
2024-10-21 21:47:39 -0300

    primera preentrega back 3

---
+++ src/services/mocking.js
@@ -0,0 +1,38 @@
+import {faker} from "@faker-js/faker"; 
+import { createHash } from "../utils/index.js";
+
+class MockingService {
+    static async generateMockingUsers(num){
+        const users = []; 
+
+        for (let i = 0; i < num; i++) {
+            users.push({
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: await createHash("coder123"),
+                role: faker.helpers.arrayElement(["user", "admin"]),
+                pets: []
+            })
+        }
+        return users; 
+    }
+
+    static async generateMockingPets(num){
+        const pets = []; 
+        const species = ['perro', 'gato', 'conejo','tortuga'];
+
+        for (let i = 0; i < num; i++) {
+            pets.push({
+                name: faker.animal.dog(),
+                specie: faker.helpers.arrayElement(species),
+                adopted: false,
+                birthDate: faker.date.past(),
+                image: "../public/img/coderDog.jpg"
+            })
+        }
+        return pets; 
+    }
+}
+
+export default MockingService; 
